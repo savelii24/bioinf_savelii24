45173fdeaa2239c6ea41cf188a8f3fe2bc1ad3e3
savelii <admin@savelii.local>
2024-10-04 13:06:55 -0700

    Add function in file filter_fastq.py

---
+++ bioinf_ut/filter_fastq.py
@@ -0,0 +1,22 @@
+from modules_4main_script import module_for_filter_fastq
+def filter_fastq(fastq_file, gc_bounds=(0, 100), length_bounds=(0, 2 ** 32), quality_threshold=0):
+    result_filtering = {}
+    for seq_name, seq, quality in fastq_file.items():
+        result_bounds = module_for_filter_fastq.length_bounds(seq)
+        result_quality = module_for_filter_fastq.quality_threshold(quality)
+        length_seq = len(seq)
+        if isinstance(gc_bounds, int):
+            gc_bounds_l, gc_bounds_u = 0, gc_bounds
+        else:
+            gc_bounds_l, gc_bounds_u = gc_bounds
+        if isinstance(length_bounds, int):
+            length_bounds_l, length_bounds_u = 0, length_bounds
+        else:
+            length_bounds_l, length_bounds_u = length_bounds
+
+        if ((result_bounds>gc_bounds_l and result_bounds<gc_bounds_u) and
+                (result_quality>quality_threshold) and
+                (length_seq>length_bounds_l and length_seq<length_bounds_u)):
+            result_filtering[seq_name] = (seq, quality)
+    return result_filtering
+
